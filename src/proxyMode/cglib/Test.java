47e3ea15d045d788c1ca3697984f7ef675c97213
hexixiang <137125668@qq.com>
2019-04-30 16:09:07 +0800

    提交

--- src/proxyMode/cglib/Test.java
+++ src/proxyMode/cglib/Test.java
@@ -3,6 +3,6 @@ package proxyMode.cglib;
 public class Test {
     public static void main(String[] args) {
         UserDao proxyInstance = (UserDao) new CgLibProxy(new UserDao()).getProxyInstance();
-        proxyInstance.save();
+        proxyInstance.save();//
     }
 }
